0a9e8d1bc06465bd56c2ba5e793fc8c210419453
Ryan <redryma@icloud.com>
2021-10-11 09:30:02 -0400

    Updates

---
+++ Assets/Scripts/Audio/MusicTicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTicker : MonoBehaviour
+{
+
+    public static AudioClip currentTrack;
+    public static AudioSource musicTicker;
+
+    void Awake()
+    {
+        musicTicker = GameObject.Find("WorldMusic").GetComponent<AudioSource>();
+    }
+
+    public static void ChangeTrack(AudioClip clip)
+    {
+        if (currentTrack != clip)
+        {
+            musicTicker.Stop();
+            currentTrack = clip;
+            musicTicker.clip = currentTrack;
+            musicTicker.Play();
+        }
+    }
+
+    public static void StopMusic()
+    {
+        musicTicker.Stop();
+    }
+
+    public static void PlayMusic()
+    {
+        musicTicker.Play();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<AudioClip> overworldDayTracks = new List<AudioClip>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
