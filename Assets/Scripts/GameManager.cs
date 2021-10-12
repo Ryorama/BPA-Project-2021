fd953cf8e8f8b90024eecc1fdff2b9c1cde616c9
Ryan <redryma@icloud.com>
2021-10-11 21:12:40 -0400

    Music changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TerrainEngine2D;
 
 public class GameManager : MonoBehaviour
 {
 
     public List<AudioClip> overworldDayTracks = new List<AudioClip>();
+    public List<AudioClip> overworldNightTracks = new List<AudioClip>();
+
+    public static bool isDaytime;
+
+    public Text fps;
+
     public GameObject invObject;
     public bool isInventoryOpen;
+    public static World world;
+
+    
 
     void Awake()
     {
-        
+        world = GameObject.Find("World").GetComponent<World>();
     }
 
     void Update()
@@ -29,9 +40,38 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        if (overworldDayTracks.Count > 0 && !MusicTicker.musicTicker.isPlaying)
+        if (world.TimeOfDay > 8 && world.TimeOfDay < 20)
         {
-            MusicTicker.ChangeTrack(overworldDayTracks[Random.Range(0, overworldDayTracks.Count - 1)]);
+            if (!isDaytime && overworldDayTracks.Count > 0)
+            {
+                MusicTicker.ChangeTrack(overworldDayTracks[Random.Range(0, overworldDayTracks.Count - 1)]);
+            }
+
+            if (!isDaytime)
+            {
+                isDaytime = true;
+            }
+
+            if (overworldDayTracks.Count > 0 && !MusicTicker.musicTicker.isPlaying)
+            {
+                MusicTicker.ChangeTrack(overworldDayTracks[Random.Range(0, overworldDayTracks.Count - 1)]);
+            }
+        } else
+        {
+             if (isDaytime && overworldNightTracks.Count > 0)
+             {
+                MusicTicker.ChangeTrack(overworldNightTracks[Random.Range(0, overworldNightTracks.Count - 1)]);
+             }
+
+            if (isDaytime)
+            {
+                isDaytime = false;
+            }
+
+            if (overworldNightTracks.Count > 0 && !MusicTicker.musicTicker.isPlaying)
+            {
+                MusicTicker.ChangeTrack(overworldNightTracks[Random.Range(0, overworldNightTracks.Count - 1)]);
+            }
         }
     }
 }
